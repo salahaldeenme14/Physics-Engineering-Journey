a461abda5fe0fe0f1507d86f4a8959298fa013d2
salahaldeenme14 <salah.aldeen.me14@gmail.com>
2026-06-13 12:02:28 +0800

    first save

---
+++ simulation.py
@@ -0,0 +1,9 @@
+# Experiment parameters
+ambient_temp = 20.0    # Room temperature in Celsius
+initial_temp = 85.0    # Starting temperature of the object
+cooling_rate = 0.05    # The constant 'k' 
+
+print("System initialized.")
+print("The room temperature is set to:", ambient_temp)
+
+
